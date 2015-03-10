783a37bd39a82e9981cc609e80fe1e5127440a30
iMaikL <maik.leenards@hva.nl>
2015-03-10 13:41:29 +0100

    Filteren attracties
    Uit de JSON data worden alleen attracties laten zien. Wachttijden worden laten zien samen met nettere attractie namen. Er worden nu maximaal 10 attracties getoond.

--- src/app.js
+++ src/app.js
@@ -9,17 +9,64 @@
 var UI = require('ui');
 var ajax = require('ajax');
 var Vector2 = require('vector2');
-
-var parseFeed = function(data, quantity) {
+var i = 0;
+var parseFeed = function(data, quantity, type) {
   var items = [];
-  for(var i = 0; i < quantity; i++) {
+  while(items.length < quantity || data.length < i) {
+    i++;
+    
+    console.log(data.AttractionInfo.length);
+    
+    if(type != data.AttractionInfo[i].Type) {
+      continue;
+    }
+    
     // Always upper case the description string
     var attraction = data.AttractionInfo[i].Id;
+    
+    //Hernoem attracties met nettere namen
+    switch(data.AttractionInfo[i].Id) {
+      case 'stoomtreinr':
+        attraction = 'Stoomtreintjes';
+        break;
+      case 'devliegendehollander':
+        attraction = 'Vliegende Hollander';
+        break;
+      case 'jorisendedraak':
+        attraction = 'Joris en de Draak';
+        break;
+      case 'carnavalfestival':
+        attraction = 'Carnaval Festival';
+        break;
+      case 'doudetuffer':
+        attraction = 'De Oude Tuffer';
+        break;
+      case 'halvemaen':
+        attraction = 'Halve Maen';
+        break;
+      case 'baron1898':
+        attraction = 'Baron 1898';
+        break;
+    }
+    
     attraction = attraction.charAt(0).toUpperCase() + attraction.substring(1);
 
     // Get waitingtimes
-    var waitingtime = data.AttractionInfo[i].Type;
-    //waitingtime = waitingtime.substring(waitingtime.indexOf('-') + 1, waitingtime.indexOf(':') + 3);
+    var waitingtime = data.AttractionInfo[i].State;
+    
+    // If statement. Als de attractie gesloten is, laat "gesloten" zien. Anders laat de wachttijd zien.
+    if (data.AttractionInfo[i].State == 'gesloten') {
+      waitingtime = data.AttractionInfo[i].State;
+    }else{
+      waitingtime = data.AttractionInfo[i].WaitingTime + " minutes";
+    }
+    // If statement. Als de attractie geen WaitingTime heeft, laat "State" zien.
+    if (!data.AttractionInfo[i].WaitingTime) {
+      waitingtime = data.AttractionInfo[i].State;
+    }
+    
+    // Always upper case the description string
+    waitingtime = waitingtime.charAt(0).toUpperCase() + waitingtime.substring(1);
 
     // Add to menu items array
     items.push({
@@ -64,7 +111,7 @@ ajax(
     console.log("Successfully fetched wachttijden data!");
     
     // Create an array of Menu items
-    var menuItems = parseFeed(data, 10);
+    var menuItems = parseFeed(data, 10, 'Attraction');
     
     // Check the items are extracted OK
     for(var i = 0; i < menuItems.length; i++) {
