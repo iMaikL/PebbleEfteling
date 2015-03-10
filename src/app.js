b3acda0eeb2b10ed21b96fe3f8879238d1238ac7
iMaikL <maik.leenards@hva.nl>
2015-03-10 01:03:02 +0100

    Menu toegevoegt met verschillende attracties
    Ik snap nog niet alle dingen met de i'tjes, maar daar ga ik zeker nog naar kijken. De wachttijd data moet staan waar het type nu staat, maar met die data kreeg ik alleen null objecten. Ik moet dus wat verzinnen om daar de tijd of "Gesloten" te hebben staan...

--- src/app.js
+++ src/app.js
@@ -8,50 +8,83 @@
 
 var UI = require('ui');
 var ajax = require('ajax');
+var Vector2 = require('vector2');
 
-// Create the Loading Card with title and subtitle
-var card = new UI.Card({
-  //title:'Efteling',
-  subtitle:'Ophalen wachttijden...'
+var parseFeed = function(data, quantity) {
+  var items = [];
+  for(var i = 0; i < quantity; i++) {
+    // Always upper case the description string
+    var attraction = data.AttractionInfo[i].Id;
+    attraction = attraction.charAt(0).toUpperCase() + attraction.substring(1);
+
+    // Get waitingtimes
+    var waitingtime = data.AttractionInfo[i].Type;
+    //waitingtime = waitingtime.substring(waitingtime.indexOf('-') + 1, waitingtime.indexOf(':') + 3);
+
+    // Add to menu items array
+    items.push({
+      title:attraction,
+      subtitle:waitingtime
+    });
+  }
+
+  // Finally return whole array
+  return items;
+};
+
+// Splash screen while waiting for data
+var splashWindow = new UI.Window();
+
+// Text element to inform user
+var text = new UI.Text({
+  position: new Vector2(0, 0),
+  size: new Vector2(144.168),
+  text: 'Downloading wachttijden...',
+  font: 'GOTHIC_28_BOLD',
+  color: 'black',
+  textOverflow: 'wrap',
+  textAlign: 'center',
+  backgroundColor: 'white'
 });
 
-card.banner('images/efteling_banner.png');
-card.style('small');
+// Add to splashWindow and show
+splashWindow.add(text);
+splashWindow.show();
 
-// Display the Card
-card.show();
-
-//END OF LOADING CARD//
-//START OF WEATHER DATA CARD//
-
-// Construct URL
-var apiKey = '...';
-var URL = 'api.eftelstats.nl/V1/wachttijden?key=' + apiKey;
+//START OF DATA//
 
 // Make the request
 ajax(
   {
-    url: URL,
+    url: 'http://www.nickd.nl/maik/efteling.php',
     type: 'json'
   },
   function(data) {
     // Success!
     console.log("Successfully fetched wachttijden data!");
+    
+    // Create an array of Menu items
+    var menuItems = parseFeed(data, 10);
+    
+    // Check the items are extracted OK
+    for(var i = 0; i < menuItems.length; i++) {
+      console.log(menuItems[i].title + ' | ' + menuItems[i].subtitle);
+    }
 
-    // Extract data
-    var location = data.name;
-    var temperature = Math.round(data.main.temp - 273.15) + "C";
+    // Construct Menu to show to user
+    var resultsMenu = new UI.Menu({
+      sections: [{
+        title: 'Current Waitingtimes',
+        items: menuItems
+      }]
+    });
 
-    // Always upper-case first letter of description
-    var description = data.weather[0].description;
-    description = description.charAt(0).toUpperCase() + description.substring(1);
-
-    // Show to user
-    card.subtitle(location + ", " + temperature);
-    card.body(description);
+    // Show the Menu, hide the splash
+    resultsMenu.show();
+    splashWindow.hide();   
   },
   function(error) {
     // Failure!
-    console.log('Failed fetching wachttijden data: ' + error);
+    console.log('Download failed' + error);
   }
 );
