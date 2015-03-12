1867a854e11e6c58bd8443e2b3767363b4b3b08e
iMaikL <maik.leenards@hva.nl>
2015-03-12 01:06:05 +0100

    Grote update
    Ik had een bug die mijn hele app liet crashen > Gefixt
    Hoofdmenu toegevoegd, doorlinken naar attracties, shows of horeca. 
    Informatie item toegevoegd met indicator voor drukte en openingstijden.
    Ik moet nog ergens een disclaimer toevoegen in de app.

--- src/app.js
+++ src/app.js
@@ -9,23 +9,21 @@
 var UI = require('ui');
 var ajax = require('ajax');
 var Vector2 = require('vector2');
-var i = 0;
 var parseFeed = function(data, quantity, type) {
   var items = [];
-  while(items.length < quantity || data.length < i) {
-    i++;
+  
+  for(var i=0; i < data.AttractionInfo.length; i++){
+    var AttractionInfo = data.AttractionInfo[i];
     
-    console.log(data.AttractionInfo.length);
-    
-    if(type != data.AttractionInfo[i].Type) {
+    //console.log(JSON.stringify(AttractionInfo));
+    if(type != AttractionInfo.Type) {
       continue;
     }
     
-    // Always upper case the description string
-    var attraction = data.AttractionInfo[i].Id;
+    var attraction = AttractionInfo.Id;
     
     //Hernoem attracties met nettere namen
-    switch(data.AttractionInfo[i].Id) {
+    switch(AttractionInfo.id) {
       case 'stoomtreinr':
         attraction = 'Stoomtreintjes';
         break;
@@ -49,20 +47,21 @@ var parseFeed = function(data, quantity, type) {
         break;
     }
     
+    // Always upper case the description string
     attraction = attraction.charAt(0).toUpperCase() + attraction.substring(1);
 
     // Get waitingtimes
-    var waitingtime = data.AttractionInfo[i].State;
+    var waitingtime = AttractionInfo.State;
     
     // If statement. Als de attractie gesloten is, laat "gesloten" zien. Anders laat de wachttijd zien.
-    if (data.AttractionInfo[i].State == 'gesloten') {
-      waitingtime = data.AttractionInfo[i].State;
+    if (AttractionInfo.State == 'gesloten') {
+      waitingtime = AttractionInfo.State;
     }else{
-      waitingtime = data.AttractionInfo[i].WaitingTime + " minutes";
+      waitingtime = AttractionInfo.WaitingTime + " minutes";
     }
     // If statement. Als de attractie geen WaitingTime heeft, laat "State" zien.
-    if (!data.AttractionInfo[i].WaitingTime) {
-      waitingtime = data.AttractionInfo[i].State;
+    if (!AttractionInfo.WaitingTime) {
+      waitingtime = AttractionInfo.State;
     }
     
     // Always upper case the description string
@@ -86,7 +85,7 @@ var splashWindow = new UI.Window();
 var text = new UI.Text({
   position: new Vector2(0, 0),
   size: new Vector2(144.168),
-  text: 'Downloading wachttijden...',
+  text: 'Ophalen wachttijden...',
   font: 'GOTHIC_28_BOLD',
   color: 'black',
   textOverflow: 'wrap',
@@ -107,28 +106,93 @@ ajax(
     type: 'json'
   },
   function(data) {
+    console.log(JSON.stringify(data));
     // Success!
     console.log("Successfully fetched wachttijden data!");
     
     // Create an array of Menu items
-    var menuItems = parseFeed(data, 10, 'Attraction');
+    var attractionItems = parseFeed(data, 32, 'Attraction');
+    var showItems = parseFeed(data, 8, 'Show');
+    var horecaItems = parseFeed(data, 32, 'Horeca');
     
-    // Check the items are extracted OK
-    for(var i = 0; i < menuItems.length; i++) {
-      console.log(menuItems[i].title + ' | ' + menuItems[i].subtitle);
-    }
-
-    // Construct Menu to show to user
-    var resultsMenu = new UI.Menu({
+    // Construct main menu
+    var mainMenu = new UI.Menu({
       sections: [{
-        title: 'Current Waitingtimes',
-        items: menuItems
+        title: 'Main Menu',
+        items: [{
+          title: 'Attracties',
+          icon: 'images/efteling_logo.png'
+        }, {
+          title: 'Shows',
+          icon: 'images/icon_ticket.png'
+        }, {
+          title: 'Horeca',
+          icon: 'images/icon_coffee.png'
+        }, {
+          title: 'Park Info',
+          icon: 'images/icon_informatie.png'
+        }]
       }]
     });
+    
+    //Variabelen voor gebruik in cards en menus 
+    var drukte = data.OpeningHours.BusyIndication;
+    drukte = drukte.charAt(0).toUpperCase() + drukte.substring(1);
+    
+    var OpenVan = data.OpeningHours.HourFrom;
+    OpenVan = OpenVan.substring(OpenVan.indexOf('T') + 1, OpenVan.indexOf(':') + 3);
+    var OpenTot = data.OpeningHours.HourTo;
+    OpenTot = OpenTot.substring(OpenTot.indexOf('T') + 1, OpenTot.indexOf(':') + 3);
+    
+    
+    var openingstijdPark = OpenVan + ' tot ' + OpenTot;
+    
+    // Construct Menu to show to user when attractions is chosen in main menu
+    var resultsMenu = new UI.Menu({
+      sections: [{
+        title: 'Huidige Wachttijden',
+        items: attractionItems
+      }]
+    });
+    // Construct Menu to show to user when shows is chosen in main menu
+    var showsMenu = new UI.Menu({
+      sections: [{
+        title: 'Huidige Wachttijden',
+        items: showItems
+      }]
+    });
+    // Construct Menu to show to user when horeca is chosen in main menu
+    var horecaMenu = new UI.Menu({
+      sections: [{
+        title: 'Huidige Wachttijden',
+        items: horecaItems
+      }]
+    });
+    // Construct Menu to show to user info about Efteling
+    var aboutMenu = new UI.Card({
+      title: 'Park Info',
+      subtitle: drukte,
+      body: openingstijdPark
+    });
 
-    // Show the Menu, hide the splash
+    // Show the mainMenu, hide the splash
+    splashWindow.hide(); 
+    mainMenu.show();
+    
+    // Add an action for SELECT
+    mainMenu.on('select', function(e) {
+    if(e.itemIndex === 0) {
     resultsMenu.show();
-    splashWindow.hide();   
+    } else if(e.itemIndex == 1) {
+    showsMenu.show();
+    } else if(e.itemIndex == 2) {
+    horecaMenu.show();
+    } else if(e.itemIndex == 3) {
+    aboutMenu.show();
+    }
+      
+    });
+    
   },
   function(error) {
     // Failure!
